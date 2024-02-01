cf3a18ccf617994127e288c6aaaa74624d83b8d5
Ritwik Ghosh <141362533+ritwik-ghosh09@users.noreply.github.com>
2024-02-01 15:06:22 +0100

    Update ex02_diffusion.py

--- ex02_diffusion.py
+++ ex02_diffusion.py
@@ -17,7 +17,7 @@ def linear_beta_schedule(beta_start, beta_end, timesteps):
 def cosine_beta_schedule(timesteps, s=0.008):
 
     'cosine schedule as proposed in https://arxiv.org/abs/2102.09672'
-    # TODO (2.3): Implement cosine beta/variance schedule as discussed in the paper mentioned above
+    # Implementing cosine beta/variance schedule as discussed in the paper mentioned above
     T = timesteps
     t = torch.linspace(0, T, T+1)
     alphas_cumprod = torch.cos(((t / T) + s) / (1 + s) * torch.pi * 0.5) ** 2       # f(t)
@@ -36,7 +36,7 @@ def cosine_beta_schedule(timesteps, s=0.008):
 
 def sigmoid_beta_schedule(beta_start, beta_end, timesteps):   # sigmoidal beta schedule - following a sigmoid function
 
-    # TODO (2.3): Implement a sigmoidal beta schedule. Note: identify suitable limits of where you want to sample the sigmoid function.
+    # Implementing a sigmoidal beta schedule. 
     # Note that it saturates fairly fast for values -x << 0 << +x
     T = timesteps
     t = torch.linspace(0, T, T)
@@ -53,7 +53,7 @@ def sigmoid_beta_schedule(beta_start, beta_end, timesteps):   # sigmoidal beta s
 
 class Diffusion:
 
-    # TODO (2.4): Adapt all methods in this class for the conditional case. You can use y=None to encode that you want to train the model fully unconditionally.
+    # Adapting all methods in this class for the conditional case. You can use y=None to encode that you want to train the model fully unconditionally.
                         # 100,       my_scheduler,        32
     def __init__(self, timesteps, get_noise_schedule, img_size, device="cuda"):
         """
@@ -64,15 +64,13 @@ class Diffusion:
         self.img_size = img_size    # 32
         self.device = device
 
-        # define beta schedule
+        # defining beta schedule
         self.betas = torch.Tensor(get_noise_schedule(self.timesteps))     # (0.001.......100 equally spaced divisions till......0.02)
 
-        # TODO (2.2): Compute the central values for the equation in the forward pass already here so you can quickly use them in the forward pass.
-        # Note that the function torch.cumprod may be of help
-
-        # define alphas
+        # Computing the central values for the equation in the forward pass already here in order to quickly use them in the forward pass.
+        # defining alphas
         self.alphas = torch.Tensor(1. - self.betas)
-        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)    #TODO
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)    
         alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0)    #since the first element will be 1
         self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
 
@@ -89,7 +87,7 @@ class Diffusion:
 
     @torch.no_grad()
     def p_sample(self, model, x, t, t_index, y=None, p_uncond=None, cfg_scale=None):
-        # TODO (2.2): implement the reverse diffusion process of the model for (noisy) samples x and timesteps t. Note that x and t both have a batch dimension
+        # Implementing the reverse diffusion process of the model for (noisy) samples x and timesteps t. x and t both have a batch dimension
         x = x.to(torch.device('cuda'))
         t = t.to(torch.device('cuda'))
         y = y.to(torch.device('cuda'))
@@ -109,7 +107,9 @@ class Diffusion:
 
         model_mean = sqrt_recip_alphas_t * (
         x - (betas_t * final_pred_noise / sqrt_one_minus_alphas_cumprod_t))     # Equation 11 in the paper
-        # Use our model (noise predictor) to predict the mean
+        
+        
+        # Using the model (noise predictor) to predict the mean
 
         if t_index == 0:
             return model_mean
@@ -117,13 +117,13 @@ class Diffusion:
         else:
             z_noise = torch.randn_like(x)
             posterior_variance_t = extract(self.posterior_variance, t, x.shape)
-            return model_mean + (torch.sqrt(posterior_variance_t) * z_noise)  # TODO (2.2): Returns the image at timestep t-1 -> x_t-1.
+            return model_mean + (torch.sqrt(posterior_variance_t) * z_noise)  # Returns the image at timestep t-1 -> x_t-1.
 
 
     # Algorithm 2 (including returning all images)
     @torch.no_grad()
     def sample(self, model, image_size, batch_size=16, channels=3, y=None, p_uncond=None, cfg_scale=None):     #samples from individual batches
-        # TODO (2.2): Implement the full reverse diffusion loop from random noise to an image, iteratively ''reducing'' the noise in the generated image.
+        # Implementing the full reverse diffusion loop from random noise to an image, iteratively ''reducing'' the noise in the generated image.
 
         x_T = torch.randn(batch_size, channels, image_size, image_size)
         x_gen = []
@@ -134,15 +134,15 @@ class Diffusion:
             x_gen.append(x_T.cpu().numpy())
             time.append(idx)
         print(f'timesteps: {time}')
-        return np.array(x_gen)   # TODO (2.2): Returns batches of generated images throughout the reverse timesteps
+        return np.array(x_gen)   # Returning batches of generated images throughout the reverse timesteps
 
 
     # forward diffusion (using the nice property)
     def q_sample(self, x_zero, t, noise=None):
-        # TODO (2.2): Implement the forward diffusion process using the beta-schedule defined in the constructor; if noise is None, you will need to create a new noise vector, otherwise use the provided one.
+        # Implementing the forward diffusion process using the beta-schedule defined in the constructor
 
         self.noise = noise
-        if self.noise is None:
+        if self.noise is None:    # if noise is None, you will need to create a new noise vector, otherwise use the provided one.
             self.noise = torch.randn_like(x_zero)
 
         sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, t, x_zero.shape)  #gets corr. values for indices in t
@@ -154,7 +154,8 @@ class Diffusion:
 
     def p_losses(self, denoise_model, x_zero, t, *, loss_type="l2", y=None, noise=None):   #(Unet, images, t=[batch of randomint between 0-100]  )
                         # Unet,         4D,
-        # TODO (2.2): compute the input to the network using the forward diffusion process and predict the noise using the model; if noise is None, you will need to create a new noise vector, otherwise use the provided one.
+       
+        # Computing the input to the network using the forward diffusion process and predicting the noise using the model
 
         inpT_Unet = self.q_sample(x_zero, t, noise)
 
